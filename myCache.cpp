296ec6fda99e6f71c6e48922c011c822fe54adaa
yy225 <yy225@duke.edu>
2018-11-24 15:18:57 -0500

    new format

--- myCache.cpp
+++ myCache.cpp
@@ -30,7 +30,8 @@ int main(int argc, char ** argv) {
                capacity,
                mode[0],
                allocOnWrMiss,
-               replaceAlg);
+               replaceAlg,
+               false);
   Cache cache2(hitTime,
                DRAMAccessTime,
                associativity,
@@ -38,19 +39,65 @@ int main(int argc, char ** argv) {
                capacity,
                mode[1],
                allocOnWrMiss,
-               replaceAlg);
+               replaceAlg,
+               false);
   std::ifstream ifs;
   ifs.open(argv[1], std::ifstream::in);
   std::string command;
   while (getline(ifs, command)) {
     parser.readCommand(command);
-    cache1.operation(
-        parser.getCmdType(), parser.getTag(), parser.b2D(parser.getSetid()), parser.getAddress());
-    cache2.operation(
-        parser.getCmdType(), parser.getTag(), parser.b2D(parser.getSetid()), parser.getAddress());
+    std::string cmdType = parser.getCmdType();
+    std::string tag = parser.getTag();
+    int setid = parser.b2D(parser.getSetid());
+    std::string address = parser.getAddress();
+    cache1.operation(cmdType, tag, setid, address);
+    cache2.operation(cmdType, tag, setid, address);
   }
-  cache1.getHitRate();
-  cache2.getHitRate();
+  /*Parser parser2(capacity / blockSize, blockSize, capacity);
+  Cache cache3(hitTime,
+               DRAMAccessTime,
+               capacity / blockSize,
+               blockSize,
+               capacity,
+               mode[0],
+               allocOnWrMiss,
+               replaceAlg);
+  Cache cache4(hitTime,
+               DRAMAccessTime,
+               capacity / blockSize,
+               blockSize,
+               capacity,
+               mode[1],
+               allocOnWrMiss,
+               replaceAlg);
+  ifs.close();
+  ifs.open(argv[1], std::ifstream::in);
+  while (getline(ifs, command)) {
+    parser2.readCommand(command);
+    std::string cmdType = parser2.getCmdType();
+    std::string tag = parser2.getTag();
+    int setid = parser.b2D(parser2.getSetid());
+    std::string address = parser2.getAddress();
+    cache3.operation(cmdType, tag, setid, address);
+    cache4.operation(cmdType, tag, setid, address);
+    }*/
+  std::vector<int> res1 = cache1.getHitRate();
+  std::vector<int> res2 = cache2.getHitRate();
+  /*std::vector<int> res3 = cache3.getHitRate();
+  std::vector<int> res4 = cache4.getHitRate();
+  res1[12] = res1[9] - res3[9];
+  res1[13] = res1[10] - res3[10];
+  res1[14] = res1[11] - res3[11];
+  res1[9] = res3[9];
+  res1[10] = res3[10];
+  res1[11] = res3[11];
+  res2[12] = res2[9] - res4[9];
+  res2[13] = res2[10] - res4[10];
+  res2[14] = res2[11] - res4[11];
+  res2[9] = res4[9];
+  res2[10] = res4[10];
+  res2[11] = res4[11];*/
+  printResult(res1, res2);
   ifs.close();
   fprintf(stdout, "+-----------------------------------------------+\n");
   return EXIT_SUCCESS;
